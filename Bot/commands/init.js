90370e8563308c936a0f32e91deef33807c85238
Adiber <adiber.yt@gmail.com>
2020-02-20 22:08:53 +0100

    categories creating
    basic concept

--- Bot/commands/init.js
+++ Bot/commands/init.js
@@ -1,7 +1,45 @@
+const fs = require('fs');
+
 module.exports.run = async(message) => {
     if(!message.member.permissions.has("ADMINISTRATOR", true)) return message.reply('You do not have the rights to do that!');
-    
+
+    if(!message.guild.member(message.client.user).permissions.has("ADMINISTRATOR", true)) {
+        message.reply("Sorry mate, but I do not have enough permissions (I need to be Administrator)");
+        message.react("❌");
+        return;
+    }
+
+    Promise.all(message.guild.roles.deleteAll()).catch();
+    message.guild.channels.deleteAll();
+
     message.react("✅");
+
+    let arg = message.content.trim().split(' ')[1] || "";
+    //default setup
+    if(arg == "default" || arg == "") {
+        let setup = require('../presets/default.json');
+
+        //creating roles FIRST!
+
+        //creating the categories SECOND!
+        for(let cat in setup.categories) {
+            if(setup.categories.hasOwnProperty(cat)) {
+
+                //getting the permissions
+                let perm = setup.categories[cat].permissions;
+                for(let p in perm) {
+                    if(perm.hasOwnProperty(p)) {
+                        perm[p].id = message.guild.roles.find(val => val.name === perm[p].role).id;
+                    }
+                }
+
+                message.guild.createChannel(setup.categories[cat].name, {"type": "category", "name": setup.categories[cat].name, "position": setup.categories[cat].position, "permissionOverwrites": perm}, "", "Channel by Server Preset Bot");
+            }
+        }
+
+        //creating the channels FINAL!
+
+    }
 
     //do stuff
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Guild-Preset-Bot
 This Bot installs Server Presets, so that you have less work to do.
+
+## Commands
+* `init default`
+ ..* > This installs the default server preset to your server
