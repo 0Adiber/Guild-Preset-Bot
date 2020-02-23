d9f97ab2eb4c76730e7ddf0c455a5c11a4f33f58
Adiber <adiber.yt@gmail.com>
2020-02-23 17:51:10 +0100

    DISCORD IST SCHEIßE
    discord sortierung ist bullshit

--- Bot/commands/init.js
+++ Bot/commands/init.js
@@ -9,46 +9,59 @@ module.exports.run = async(message) => {
         return;
     }
 
-    Promise.all(message.guild.roles.deleteAll()).catch(err => console.log(err));
-    message.guild.emojis.deleteAll();
-    message.guild.channels.deleteAll();
+    //delete roles, channels and emojis
+    await Promise.all(message.guild.roles.filter(val => val.id !== message.guild.defaultRole.id).deleteAll()).catch((err) => console.log(err));
+    await Promise.all(message.guild.emojis.deleteAll()).catch((err) => console.log(err));
+    await Promise.all(message.guild.channels.deleteAll()).catch((err) => console.log(err));
+
     message.guild.createChannel("debug", {'type': 'text'}).then(async(debug) => {
 
-        debug.send("Creating... 🦄");
-
         let arg = message.content.trim().split(' ')[1] || "";
-    
+        
         let setup = {}
-    
+        
         //default setup
         if(arg == "") {
             setup = require('../presets/default.json');
         } else {
             setup = require(`../presets/${arg}.json`)
         }
-    
+        
+        debug.send("Starting... 🦄");
         if(!Object.keys(setup).length) return message.reply("This preset does not exist!");
-    
-        let pendingPromises = []
+        await sleep(3000);
 
-        //creating roles FIRST!
+
+        let pendingPromises = []
+        //CREATING roles FIRST!
+        debug.send("Creating Roles");
         for(let role in setup.roles) {
             if(setup.roles.hasOwnProperty(role)) {
                 pendingPromises.push(message.guild.createRole(setup.roles[role])
                         .catch(err => {
                             debug.send("The preset you chose has some errors! Please contact me on my Discord [gg.adiber.at] (1)")
-                            console.error(err)
+                            console.log(err)
                         }));
             }
         }
+        await Promise.all(pendingPromises);
+        pendingPromises = []
+        //UPDATING ROLE POSITIONS
+        debug.send("Updating Role Positions");
+        for(let role in setup.roles) {
+            if(setup.roles.hasOwnProperty(role)) {
+                pendingPromises.push(message.guild.roles.find(val => val.name === setup.roles[role].name).setPosition(setup.roles[role].position));
+                console.log(message.guild.roles.find(val => val.name === setup.roles[role].name).name + " -> " + setup.roles[role].position)
+            }
+        }
+
 
         await Promise.all(pendingPromises);
         pendingPromises = [];
-    
-        //creating the categories SECOND!
+        //CREATING the categories SECOND!
+        debug.send("Creating Categories");
         for(let cat in setup.categories) {
             if(setup.categories.hasOwnProperty(cat)) {
-    
                 //getting the permissions
                 let perm = setup.categories[cat].permissions;
                 for(let p in perm) {
@@ -57,18 +70,28 @@ module.exports.run = async(message) => {
                     }
                 }
     
-                pendingPromises.push(message.guild.createChannel(setup.categories[cat].name, {"type": "category", "name": setup.categories[cat].name, "position": setup.categories[cat].position, "permissionOverwrites": perm})
+                pendingPromises.push(message.guild.createChannel(setup.categories[cat].name, {"type": "category", "position": setup.categories[cat].position, "name": setup.categories[cat].name, "permissionOverwrites": perm})
                         .catch(err => {
-                            debug.send("The preset you chose has some errors! Please contact me on my Discord [gg.adiber.at] (1)")
-                            console.error(err)
+                            debug.send("The preset you chose has some errors! Please contact me on my Discord [gg.adiber.at] (2)")
+                            console.log(err)
                         }));
             }
         }
-    
         await Promise.all(pendingPromises);
         pendingPromises = [];
+        //UPDATING category positions!
+        debug.send("Updating Category Positions");
+        for(let cat in setup.categories) {
+            if(setup.categories.hasOwnProperty(cat)) {
+                pendingPromises.push(message.guild.channels.find(val => (val.name === setup.categories[cat].name && val.type === 'category')).setPosition(setup.categories[cat].position));
+            }
+        }
 
-        //creating the channels FINAL!
+
+        await Promise.all(pendingPromises);
+        pendingPromises = [];
+        //CREATING the channels FINAL!
+        debug.send("Creating Channels");
         for(let chan in setup.channels) {
             if(setup.channels.hasOwnProperty(chan)) {
     
@@ -83,19 +106,41 @@ module.exports.run = async(message) => {
     
                 let cat = message.guild.channels.findKey(c => (c.type === "category" && c.name === setup.channels[chan].category));
     
-                message.guild.createChannel(setup.channels[chan].name, {"type": setup.channels[chan].type, "name": setup.channels[chan].name, "position": setup.channels[chan].position, "nsfw": (setup.channels[chan].nsfw || false), "parent": cat, "userLimit": (setup.channels[chan].userLimit || 0), "permissionOverwrites": perm})
+                pendingPromises.push(message.guild.createChannel(setup.channels[chan].name, {"type": setup.channels[chan].type, "name": setup.channels[chan].name, "position": setup.channels[chan].position, "nsfw": (setup.channels[chan].nsfw || false), "parent": cat, "userLimit": (setup.channels[chan].userLimit || 0), "permissionOverwrites": perm})
                         .catch(err => {
-                            debug.send("The preset you chose has some errors! Please contact me on my Discord [gg.adiber.at] (1)")
-                            console.error(err)
-                        });
+                            debug.send("The preset you chose has some errors! Please contact me on my Discord [gg.adiber.at] (3)")
+                            console.log(err)
+                        }));
             }
         }
+        await Promise.all(pendingPromises);
+        pendingPromises = [];
+        //UPDATING channel positions!
+        debug.send("Updating Channel Positions");
+        for(let chan in setup.channels) {
+            if(setup.channels.hasOwnProperty(chan)) {
+                pendingPromises.push(message.guild.channels.find(val => (val.name === setup.channels[chan].name && val.type !== 'category')).setPosition(setup.channels[chan].position));
+            }
+        }
+
+        await Promise.all(pendingPromises);
+        pendingPromises = [];
+        
+        message.guild.setAFKChannel(message.guild.channels.find(val => (val.type === 'voice' && val.name === setup.general.afkChannel)))
+                        .catch((err) => console.log(err));
+        message.guild.setAFKTimeout((setup.general.afkTimeout || 5*60))
+                        .catch((err) => console.log(err));
     });
     
 }
+
+async function sleep(msec) {
+    return new Promise(resolve => setTimeout(resolve, msec));
+}
+
 module.exports.help = {
     name: 'init',
     description: 'Apply a preset to your server',
-    usage: 'clear',
+    usage: 'init [preset-name]',
     aliases: []
 }
--- README.md
+++ README.md
@@ -6,9 +6,9 @@ This Bot installs Server Presets, so that you have less work to do.
 
 ## Commands
 
-*  `init default` > This installs the default server preset to your server
-
-  
+* `init <preset>` > Installs the given Preset onto your server
+* `save <name>` > gives you a Preset file of the server
+* `clear` > wipes the server  
 
 ------  
 
@@ -21,16 +21,45 @@ This Bot installs Server Presets, so that you have less work to do.
 * `name` ⇒ is the Name
 * `color` ⇒ ColorResolvable. I recommend HEX Strings. [COLOR](https://discord.js.org/#/docs/main/stable/typedef/ColorResolvable)
 * `hoist` ⇒ whether to display role members separately from online members
-* `position` ⇒ where to position in the member bar on the right
+* `position` ⇒ where to position in the member bar on the right (set to the calculatedPosition)
 * `mentionable` ⇒ whether you can mention this Role with '@Role'
 * `permissions` ⇒ PermissionResolvable. I recommend using FLAGS represented in an Array : [FLAGS](https://discord.js.org/#/docs/main/stable/class/Permissions?scrollTo=s-FLAGS)
 
+### Categories
+
+`{"name": "admin", "calculatedPosition": 10, "permissions": [
+            {"deny": ["SEND_MESSAGES", "READ_MESSAGES", "VIEW_CHANNEL"], "role":"@everyone"},
+            {"allow": ["SEND_MESSAGES", "READ_MESSAGES", "VIEW_CHANNEL"], "role":"Admin"}
+        ]}`
+
+* `name` ⇒ is the Name
+* `calculatedPosition` ⇒ the Positon in the Server
+* `permissions` ⇒ The Permissions for each Role; again: I recommend doing it with the FLAGS
+
+### Channels
+
+`{"name": "talk", "type": "voice", "category": "general", "nsfw": false, "calculatedPosition": 0, "userLimit": 10, "permissions": []}}`
+
+* `name` ⇒ is the Name
+* `type` ⇒ whether it is a voice or text channel
+* `category` ⇒ the Parent category of the channel
+* `nsfw` ⇒ whether the channel is nsfw or not | only applicable for text
+* `calculatedPosition` ⇒ the Positon in the Server
+* `userLimit` ⇒ maximum amount of users that can connect at the same time | only applicable for voice
+* `permissions` ⇒ The Permissions for each Role; again: I recommend doing it with the FLAGS
+
+### General
+
+`{"afkChannel": "talk", "afkTimeout": 1000}`
+
+* `afkChannel` ⇒ the AFK channel (null if none)
+* `afkTimeout` ⇒ how many seconds, until a user is registered as "AFK"; allowed values: (60, 300, 900, 1800, 3600)
 
 -----
 
 ## TODO
 - [x] Load Preset onto Server
-- [ ] Existing Server to '.json'-Preset
+- [x] Existing Server to '.json'-Preset
 - [ ] Emoji Datenbank für Emoji Presets
     - [ ] einzeln
     - [ ] als Set
